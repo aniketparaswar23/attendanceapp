4aeabb0bb8105e613a13fd948bf54aeffbdda8e7
Aniket Paraswar <aniket.paraswar23@spit.ac.in>
2024-04-18 08:37:18 +0530

    Added Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
 
 void main() {
   runApp(const MyApp());
